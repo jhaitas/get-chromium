346cc1936eb5b87d29cdb7a116a640911a9679c4
John Haitas <john@haitas.com>
2010-03-27 19:25:42 -0500

    supporting linux now

--- latestChromiumURL.py
+++ latestChromiumURL.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python
 
 import urllib
+import platform
 
-baseURL = 'http://build.chromium.org/buildbot/snapshots/chromium-rel-mac/'
-latestURL = baseURL+'LATEST'
+system = platform.system()
+arch = platform.machine()
+
+
+OS = { 
+	'Linux' : 'linux',
+	'Darwin' : 'mac'
+} [system]
+if OS=='linux' and arch=='x86_64':
+	extraArch = '-64'
+else:
+	extraArch = ''
+
+chromiumRelease = OS+extraArch
+archiveName = 'chrome-'+OS+'.zip'
+
+baseURL = 'http://build.chromium.org/buildbot/snapshots/'
+releaseURL = baseURL+'chromium-rel-'+chromiumRelease+'/'
+latestURL = releaseURL+'LATEST'
 latestREV = urllib.urlopen(latestURL).read()
-latestChromium = baseURL+latestREV+'/chrome-mac.zip'
+latestChromium = releaseURL+latestREV+'/'+archiveName
 
 print latestChromium
 
